758a92f0d5f8dd09a9234f18f7a20f38f9995dc0
salih18 <ss@pop-os.localdomain>
2020-04-17 23:29:23 +0200

    initial blogs

--- portfolio-salih/components/layouts/BaseLayout.js
+++ portfolio-salih/components/layouts/BaseLayout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Header from "./../shared/Header";
 
 const BaseLayout = ({
@@ -9,16 +10,30 @@ const BaseLayout = ({
   headerType = "default",
 }) => {
   return (
-    <div className="layout-container">
-      <Header
-        className={`port-nav-${headerType}`}
-        user={user}
-        isAuthenticated={isAuthenticated}
-      />
-      <main className={`cover ${className}`}>
-        <div className="wrapper">{children}</div>
-      </main>
-    </div>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <div className="layout-container">
+        <Header
+          className={`port-nav-${headerType}`}
+          user={user}
+          isAuthenticated={isAuthenticated}
+        />
+        <main className={`cover ${className}`}>
+          <div className="wrapper">{children}</div>
+        </main>
+      </div>
+    </>
   );
 };
 
